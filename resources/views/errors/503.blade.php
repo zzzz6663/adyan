ef0487191d80e6052d4e144d197b16aa7accb554
naser <na3r.jafari@gmail.com>
2022-05-09 05:54:28 +0430

    b80

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,25 +1,145 @@
-@extends('master.main')
 
 
-@php($side = true)
-@php($header = true)
-@php($footer = true)
+<!DOCTYPE html>
+<!--
+Template Name: Metronic - Bootstrap 4 HTML, React, آنگولار 9 & VueJS Admin داشبورد تم
+Author: Keenتمs
+Website: http://www.keenthemes.com/
+Contact: support@keenthemes.com
+Follow: www.twitter.com/keenthemes
+Dribbble: www.dribbble.com/keenthemes
+Like: www.facebook.com/keenthemes
+Purchase: https://www.rtl-theme.com/metronic-admin-html-template/
+Renew Support: https://www.rtl-theme.com/metronic-admin-html-template/
+License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
+-->
+<html direction="rtl" dir="rtl" style="direction: rtl" >
+    <!--begin::Head-->
+    <head><base href="../../../">
+        <meta charset="utf-8"/>
+        <title>مترونیک | صفحه خطا - 6</title>
+        <meta name="description" content=""/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+
+        <!--begin::Fonts-->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700"/>        <!--end::Fonts-->
 
 
-@section('script')
-<script src="/assets/js/pages/custom/login/login-general.js?v=7.0.6"></script>
-@endsection
-@section('main')
+                    <!--begin::Page Custom Styles(used by this page)-->
+                             <link href="/assets/css/pages/error/error-6.rtl.css?v=7.0.6" rel="stylesheet" type="text/css"/>
+                        <!--end::Page Custom Styles-->
 
-<div class="error error-6 d-flex flex-row-fluid bgi-size-cover bgi-position-center" style="background-image: url(/assets/media/error/bg6.jpg);">
-    <!--begin::Content-->
-    <div class="d-flex flex-column flex-row-fluid text-center">
-        <h1 class="error-title font-weight-boldest text-white mb-12" style="margin-top: 12rem;">متاسفیم !...</h1>
-        <p class="display-4 font-weight-bold text-white">
-            به نظر می رسد مشکلی پیش آمده است.</br>
-            ما روی آن کار می کنیم
-        </p>
-    </div>
-    <!--end::Content-->
-</div>
-@endsection
+        <!--begin::Global تم Styles(used by all pages)-->
+                    <link href="/assets/plugins/global/plugins.bundle.rtl.css?v=7.0.6" rel="stylesheet" type="text/css"/>
+                    <link href="/assets/plugins/custom/prismjs/prismjs.bundle.rtl.css?v=7.0.6" rel="stylesheet" type="text/css"/>
+                    <link href="/assets/css/style.bundle.rtl.css?v=7.0.6" rel="stylesheet" type="text/css"/>
+                <!--end::Global تم Styles-->
+
+        <!--begin::Layout تمs(used by all pages)-->
+
+<link href="/assets/css/themes/layout/header/base/light.rtl.css?v=7.0.6" rel="stylesheet" type="text/css"/>
+<link href="/assets/css/themes/layout/header/menu/light.rtl.css?v=7.0.6" rel="stylesheet" type="text/css"/>
+<link href="/assets/css/themes/layout/brand/dark.rtl.css?v=7.0.6" rel="stylesheet" type="text/css"/>
+<link href="/assets/css/themes/layout/aside/dark.rtl.css?v=7.0.6" rel="stylesheet" type="text/css"/>        <!--end::Layout تمs-->
+
+        <link rel="shortcut icon" href="/assets/media/logos/favicon.ico"/>
+
+            </head>
+    <!--end::Head-->
+
+    <!--begin::Body-->
+    <body  id="kt_body"  class="header-fixed header-mobile-fixed subheader-enabled subheader-fixed aside-enabled aside-fixed aside-minimize-hoverable page-loading"  >
+
+    	<!--begin::Main-->
+	<div class="d-flex flex-column flex-root">
+		 <!--begin::Error-->
+ <div class="error error-6 d-flex flex-row-fluid bgi-size-cover bgi-position-center" style="background-image: url(assets/media/error/bg6.jpg);">
+ 	<!--begin::Content-->
+ 	<div class="d-flex flex-column flex-row-fluid text-center">
+ 		<h1 class="error-title font-weight-boldest text-white mb-12" style="margin-top: 12rem;">متاسفیم !...</h1>
+ 		<p class="display-4 font-weight-bold text-white">
+ 			به نظر می رسد مشکلی پیش آمده است.</br>
+ 			ما روی آن کار می کنیم
+ 		</p>
+	 </div>
+	 <!--end::Content-->
+ </div>
+ <!--end::Error-->
+	</div>
+<!--end::Main-->
+
+
+        <script>var HOST_URL  = "https://preview.keenthemes.com/metronic/theme/html/tools/preview";</script>
+        <!--begin::Global Config(global config for global JS scripts)-->
+        <script>
+            var KTAppSettings = {
+    "breakpoints": {
+        "sm": 576,
+        "md": 768,
+        "lg": 992,
+        "xl": 1200,
+        "xxl": 1400
+    },
+    "colors": {
+        "theme": {
+            "base": {
+                "white": "#ffffff",
+                "primary": "#3699FF",
+                "secondary": "#E5EAEE",
+                "success": "#1BC5BD",
+                "info": "#8950FC",
+                "warning": "#FFA800",
+                "danger": "#F64E60",
+                "light": "#E4E6EF",
+                "dark": "#181C32"
+            },
+            "light": {
+                "white": "#ffffff",
+                "primary": "#E1F0FF",
+                "secondary": "#EBEDF3",
+                "success": "#C9F7F5",
+                "info": "#EEE5FF",
+                "warning": "#FFF4DE",
+                "danger": "#FFE2E5",
+                "light": "#F3F6F9",
+                "dark": "#D6D6E0"
+            },
+            "inverse": {
+                "white": "#ffffff",
+                "primary": "#ffffff",
+                "secondary": "#3F4254",
+                "success": "#ffffff",
+                "info": "#ffffff",
+                "warning": "#ffffff",
+                "danger": "#ffffff",
+                "light": "#464E5F",
+                "dark": "#ffffff"
+            }
+        },
+        "gray": {
+            "gray-100": "#F3F6F9",
+            "gray-200": "#EBEDF3",
+            "gray-300": "#E4E6EF",
+            "gray-400": "#D1D3E0",
+            "gray-500": "#B5B5C3",
+            "gray-600": "#7E8299",
+            "gray-700": "#5E6278",
+            "gray-800": "#3F4254",
+            "gray-900": "#181C32"
+        }
+    },
+    "font-family": "Poppins"
+};
+        </script>
+        <!--end::Global Config-->
+
+    	<!--begin::Global تم Bundle(used by all pages)-->
+    	    	   <script src="/assets/plugins/global/plugins.bundle.js?v=7.0.6"></script>
+		    	   <script src="/assets/plugins/custom/prismjs/prismjs.bundle.js?v=7.0.6"></script>
+		    	   <script src="/assets/js/scripts.bundle.js?v=7.0.6"></script>
+				<!--end::Global تم Bundle-->
+
+
+            </body>
+    <!--end::Body-->
+</html>
